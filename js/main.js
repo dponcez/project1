030725bd4d0d82cedafc1a7a15e86f4931fe2f15
Damian Ponce <dponcez_git@hotmail.com>
2020-05-02 15:44:17 -0500

    Se modificó contenido en la carpeta scss en el arhivo principal

---
+++ js/main.js
@@ -0,0 +1,29 @@
+const init = () => {
+    // Variables
+    // getting the current HTML element
+    const audio = document.querySelector('.audio');
+    const progress__bar = document.querySelector('.progress--bar');
+    const progress = document.querySelector('.progress');
+    const player = document.querySelector('.player');
+    const muted = document.querySelector('.muted');
+    const slide__control = document.querySelector('input[type=range');
+    let current__text, duration__tex;
+    let paused = '<i class="fas fa-pause"></i>';
+    let played = '<i class="fas fa-play"></i>'
+
+    // Functions
+    const playPause = () => {
+        if(audio.paused){
+            audio.play();
+            player.innerHTML = paused
+        }else{
+            audio.pause();
+            player.innerHTML = played;
+        }
+    }
+
+    // Event handler
+    player.addEventListener('click', playPause);
+}
+
+window.addEventListener('load', init);
