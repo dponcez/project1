d2504721d09adb1f3b64ba8a70338f4ffeb26b52
Damian Ponce <dponcez_git@hotmail.com>
2021-02-12 20:10:43 -0500

    Proyecto 35: Image Carousel

--- day35-ImageCarousel/js/script.js
+++ day35-ImageCarousel/js/script.js
@@ -1,11 +1,11 @@
 /* Create variables that contains our HTML elements */
-const images = document.querySelector('img');
+const images = document.getElementById('gallery');
 const prevBtn = document.getElementById('prev');
 const nextBtn = document.getElementById('next');
 const gallery = document.querySelectorAll('[data-image] img');
 
 // Create index variable and Initialize it to zero ( 0 );
-const index = 0;
+let index = 0;
 
 let interval = setInterval( runAnimation, 2000)
 
@@ -20,7 +20,13 @@ function changeImage() {
     // Compare if the index value is greater than to gallery.length - 1
     // if so, set the index variable to zero
     // otherwise, set index variable less than to gallery.length - 1
+    if( index > gallery.length - 1) {
+        index = 0;
+    }else if( index < 0 ) {
+        index = gallery.length - 1
+    }
 
+    images.style.transform = `translateX( ${ -index * 500 }px)`
 }
 
 // resetInterval function 
@@ -36,4 +42,14 @@ function resetInterval() {
         - resetInterval function 
         
     * increment or decrement index variable
-*/
+*/prevBtn.addEventListener('click', () => {
+    index--;
+    changeImage();
+    resetInterval();
+})
+
+nextBtn.addEventListener('click', () => {
+    index++;
+    changeImage();
+    resetInterval();
+})
