6eb4a80d7c16f1d56d278b07112dfb3aa564e794
Damian Ponce <dponcez_git@hotmail.com>
2021-03-06 19:51:09 -0500

    Proyecto 41: Verify Account
    updated!

--- day41-VerifyAccount/js/script.js
+++ day41-VerifyAccount/js/script.js
@@ -6,10 +6,12 @@ function init(){
         code.addEventListener('keypress', ( e ) => {
             if( e.key >= 0 && e.key <= 9 ){
                 codes[ index ].value = '';
-                setTimeout(() => codes[ index + 1].focus(), 10 )
-            }else {
-                setTimeout(() => codes[ index - 1]. focus(), 10 )
+                setTimeout(() => codes[ index + 1 ].focus(), 10 )
+            }else if( e.key === 'Backspace' ) {
+                setTimeout(() => codes[ index - 1 ].focus(), 10 )
             }
         })
     })
 }
+
+init()
