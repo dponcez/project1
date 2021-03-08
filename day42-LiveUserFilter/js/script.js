6fa2be01e042755638c429bca7b20c80486d761c
Damian Ponce <dponcez_git@hotmail.com>
2021-03-07 20:23:52 -0500

    Proyecto 42: Live User Filter

---
+++ day42-LiveUserFilter/js/script.js
@@ -0,0 +1,45 @@
+function init() {
+    const result = document.getElementById('result');
+    const filter = document.querySelector('input[type=text]');
+    const userList = [];
+
+    getData();
+
+    filter.addEventListener('input', ( e ) => filterData( e.target.value ))
+
+    async function getData() {
+        const response = await fetch("https://randomuser.me/api?results=50");
+
+        const { results } = await response.json();
+
+        // Clear result
+        result.innerHTML = '';
+
+        results.forEach( user => {
+            const li = document.createElement('li');
+
+            userList.push( li );
+            li.innerHTML = `
+                <img src="${ user.picture.large }" alt="${ user.name.first }">
+                <div class="user--info">
+                    <h4>${ user.name.first } ${ user.name.last }</h4>
+                    <p>${ user.location.city }, ${ user.location.country }</p>
+                </div>
+            `;
+
+            result.appendChild( li )
+        })
+    }
+
+    function filterData( searchTerms ) {
+        userList.forEach( item => {
+            if( item.innerText.toLowerCase().includes( searchTerms.toLowerCase()) ) {
+                item.classList.remove('hide');
+            }else {
+                item.classList.add('hide');
+            }
+        })
+    }
+}
+
+init();
