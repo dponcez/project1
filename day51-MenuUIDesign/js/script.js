c1dda6dc8f883e1e0e40861d64253e630d5d8735
Damian Ponce <dponcez_git@hotmail.com>
2021-04-21 23:37:11 -0500

    Proyecto 51: Menu UI Design

---
+++ day51-MenuUIDesign/js/script.js
@@ -0,0 +1,51 @@
+function debounce( func, wait, immediate ) {
+    let timer = undefined;
+    return ( ...args ) => {
+        args = arguments;
+        let context = this;
+        let later = () => {
+            if( !immediate ) { func.apply( context, args ) }
+        }
+
+        clearTimeout( timer );
+        timer = setTimeout( later , wait );
+
+        const callNow = immediate && !timer;
+
+        if( callNow ) { func.apply( context, args ) }
+    }
+}
+
+function init() {
+    
+    const toggler = document.querySelector('.toggle');
+    const menuItems = document.querySelectorAll('.item');
+    const menuLinks = document.querySelectorAll('.link > span');
+
+    let isOpen = false;
+
+    toggler.addEventListener('click', debounce( ()=> {
+
+        if( !isOpen ) {
+            toggler.classList.toggle('open')
+
+            if( toggler && menuItems ) {
+                menuItems.forEach(( items, index  ) => {
+                    if( items.classList.contains('active') ) {
+                        items.classList.remove('active')
+                        index++
+                    }else {
+                        items.classList.add('active')
+                        index--
+                    }
+                })
+
+                menuLinks.forEach( links => {
+                    links.classList.toggle('remove')
+                })
+            }
+        }
+    }) )
+}
+
+document.addEventListener('DOMContentLoaded', init )
