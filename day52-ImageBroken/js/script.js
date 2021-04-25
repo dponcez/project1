d49fbc7ab68021515048576125ccdc04b2e61c5e
Damian Ponce <dponcez_git@hotmail.com>
2021-04-25 00:07:33 -0500

    Updating code

--- day52-ImageBroken/js/script.js
+++ day52-ImageBroken/js/script.js
@@ -17,6 +17,30 @@ function debounce( func, wait = 3000, immediate ) {
 }
 
 function init () {
+
+    const container = document.querySelector('.container');
+    const loadBanner = () => {
+        const banner = document.createElement('div');
+        banner.className = 'banner--container';
+        banner.innerHTML = `
+            <h1 class="heading">broken image</h1>
+            <main class="content">
+                <p class="description">when the image no load for any reason or may be the URL is broken, this has a default style, so in this example, we gave our styles to show something defferent in a broken image.</p>
+                <h2 class="description">the image is broken? Wait a few seconds until to load the image</h2>
+            </main>
+            <!--button class="close">x</!--button-->
+        `;
+        const button = document.createElement('button');
+        button.className = 'close';
+        button.textContent = 'x'
+        button.addEventListener('click', () => {
+            console.log('clicked')
+        })
+        banner.appendChild(button)
+        container.appendChild( banner )
+    }
+
+    loadBanner()
     
     const loadImage = () => {
         const image = document.querySelector('.image');
