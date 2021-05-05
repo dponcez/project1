572cd22ba1fec897f5b90eee03a9514362365d33
Damian Ponce <dponcez_git@hotmail.com>
2021-05-04 21:06:08 -0500

    Added new lines of code on styles.scss and script.js

--- day55-HexColorPicker/js/script.js
+++ day55-HexColorPicker/js/script.js
@@ -1,4 +1,7 @@
-function denounce( func, wait, immediate ) {
+// Returns a function that as long as continues it's execution to be invoked.
+// This function will be called after being stops it's execution for 'n' milliseconds, 
+// preventing immediately a fire handling events by the  user
+function debounce( func, wait, immediate ) {
     let timer = null;
     return ( ...args ) => {
         args = arguments;
@@ -22,7 +25,26 @@ function denounce( func, wait, immediate ) {
     const button = document.querySelector('.btn');
     const body = document.body;
 
-    button.addEventListener('click', debounce( () => {
+    function changeHexColor( ) {
+        let hexColorText = '#';
+        const hexColor = [0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f'];
         
+        hexColor.forEach( ( random, index ) => {
+            if( index < 6 ) {
+                random = Math.floor( Math.random() * hexColor.length );
+                hexColorText += hexColor[ random ]
+            }
+        })
+
+        output.innerHTML = hexColorText;
+        body.style.backgroundColor = hexColorText;
+        button.style.color = hexColorText;
+    }
+
+    // Execute the handler event to call the changeHexColor function
+    button.addEventListener('click', debounce( () => {
+        // This function will be call after pressing the button
+        changeHexColor();
+
     }, 300 ))
 })()
