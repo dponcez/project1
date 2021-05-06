dba3deaa982611f7e4a81cd51f29f6ac0a00d9fb
Damian Ponce <dponcez_git@hotmail.com>
2021-05-05 20:25:42 -0500

    Added lines of code on our files

--- day56-DigitalClock/js/script.js
+++ day56-DigitalClock/js/script.js
@@ -1,7 +1,8 @@
 ( function() {
-    const hourText = document.querySelector('.hr time');
-    const minText = document.querySelector('.min time');
-    const secText = document.querySelector('.sec time');
+    const hourHand = document.querySelector('.hr .time');
+    const minHand = document.querySelector('.min .time');
+    const secHand = document.querySelector('.sec .time');
+    const ampm = document.querySelector('.desc')
 
     function getActualTime() {
         let now = new Date();
@@ -10,10 +11,28 @@
         let secs = now.getSeconds();
 
         // get degress about our time
-        let hourDeg = (( hour / 12) * 360 ) + 90;
-        let minDeg = (( mins / 60 ) * 360 ) + 90;
-        let secDeg = (( secs / 60 ) * 360 ) + 90;
+        // let hourDeg = (( hour / 12) * 360 ) + 90;
+        // let minDeg = (( mins / 60 ) * 360 ) + 90;
+        // let secDeg = (( secs / 60 ) * 360 ) + 90;
 
+        let hour_text;
         // statements
+        if( hour > 12 ) {
+            hour_text = ( hour - 12 );
+            ampm.innerHTML = 'p.m'
+        }else {
+            hour_text = hour;
+            ampm.innerHTML = 'a.m'
+        }
+
+        let min_text = ( mins < 10 ) ? '0' + mins : mins;
+        let sec_text = ( secs < 10 ) ? '0' + secs : secs;
+
+        hourHand.innerHTML = `${ hour_text }`;
+        minHand.innerHTML = `${ min_text }`;
+        secHand.innerHTML = `${ sec_text }`
+        
     }
+
+    setInterval( getActualTime, 1000 )
 })()
