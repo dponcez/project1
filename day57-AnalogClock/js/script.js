ec22df05d3ae2d8c31d1ffde733c847c25c6fd60
Damian Ponce <dponcez_git@hotmail.com>
2021-05-07 22:10:33 -0500

    Added new lines of code on styles.scss and script.js also was changed and deleted some classes name on index.html

--- day57-AnalogClock/js/script.js
+++ day57-AnalogClock/js/script.js
@@ -1,9 +1,9 @@
 ( function () {
 
     // HTML references
-    const hourHand = document.querySelector('.hr');
-    const minHand = document.querySelector('.min');
-    const secHand = document.querySelector('.sec');
+    const hourHand = document.querySelector('.hh');
+    const minHand = document.querySelector('.mn');
+    const secHand = document.querySelector('.ss');
 
     function getActualTime() {
       const now = new Date();
@@ -15,5 +15,11 @@
       let hourDeg = (( hour / 12) * 360 ) + 90;
       let minDeg = (( mins / 60 ) * 360 ) + 90;
       let secDeg = (( secs / 60 ) * 360 ) + 90;
+
+      hourHand.style.transform = `rotate( ${ hourDeg }deg )`;
+      minHand.style.transform = `rotate( ${ minDeg }deg )`;
+      secHand.style.transform = `rotate( ${ secDeg }deg )`;
     }
+
+    setInterval( getActualTime, 1000 )
 })()
