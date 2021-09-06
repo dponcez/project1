80877adbd7d53f915a7f456bb48b98b960e2c708
Damian Ponce <dponcez_git@hotmail.com>
2021-09-06 18:17:59 -0500

    Delete web-page file

--- web-page/js/main.js
+++
@@ -1,6 +0,0 @@
-import { setScrollOnNavigation } from "../modules/sticky.js";
-import { fetchElementsFromData } from "../modules/index.js";
-
-// Call function
-setScrollOnNavigation();
-fetchElementsFromData( );
